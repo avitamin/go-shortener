8eb46aacb538b6a51e51365c95d14acec2abb470
Антонов <vitaly.antonov@altarix.ru>
2025-10-10 18:07:10 +0400

    8080 по умолчанию

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,13 +11,13 @@ func New() *Config {
 	address := os.Getenv("SERVER_ADDRESS")
 
 	if address == "" {
-		address = "localhost:8099"
+		address = "localhost:8080"
 	}
 
 	base := os.Getenv("BASE_URL")
 
 	if base == "" {
-		base = "http://localhost:8099"
+		base = "http://localhost:8080"
 	}
 
 	return &Config{
