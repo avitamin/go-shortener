ca0679d84eece691a612642e34eddea6b9c01fbe
Avi <antonovvit@gmail.com>
2025-11-04 13:22:26 +0400

    путь до файла хранилища

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,18 +8,21 @@ import (
 )
 
 type Config struct {
-	Address string `env:"SERVER_ADDRESS"`
-	BaseURL string `env:"BASE_URL"`
+	Address         string `env:"SERVER_ADDRESS"`
+	BaseURL         string `env:"BASE_URL"`
+	FileStoragePath string `env:"FILE_STORAGE_PATH"`
 }
 
 var (
-	addr string
-	base string
+	addr    string
+	base    string
+	storage string
 )
 
 func init() {
 	flag.StringVar(&addr, "a", "localhost:8080", "адрес сервера (например localhost:8080)")
 	flag.StringVar(&base, "b", "http://localhost:8080", "базовый URL (например http://localhost:8080)")
+	flag.StringVar(&storage, "f", "./storage", "путь к файлу хранилища (например ./storage)")
 }
 
 func New(withParse bool) *Config {
--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -9,42 +9,48 @@ import (
 
 func TestConfig(t *testing.T) {
 	tests := []struct {
-		name        string
-		args        []string
-		env         map[string]string
-		wantAddress string
-		wantBaseURL string
+		name                string
+		args                []string
+		env                 map[string]string
+		wantAddress         string
+		wantBaseURL         string
+		wantFileStoragePath string
 	}{
 		{
-			name:        "defaults only",
-			args:        []string{"cmd"},
-			wantAddress: "localhost:8080",
-			wantBaseURL: "http://localhost:8080",
+			name:                "defaults only",
+			args:                []string{"cmd"},
+			wantAddress:         "localhost:8080",
+			wantBaseURL:         "http://localhost:8080",
+			wantFileStoragePath: "./storage",
 		},
 		{
-			name:        "flags override defaults",
-			args:        []string{"cmd", "-a=127.0.0.1:9001", "-b=http://127.0.0.1:9001"},
-			wantAddress: "127.0.0.1:9001",
-			wantBaseURL: "http://127.0.0.1:9001",
+			name:                "flags override defaults",
+			args:                []string{"cmd", "-a=127.0.0.1:9001", "-b=http://127.0.0.1:9001", "-f=./new-storage"},
+			wantAddress:         "127.0.0.1:9001",
+			wantBaseURL:         "http://127.0.0.1:9001",
+			wantFileStoragePath: "./new-storage",
 		},
 		{
 			name: "env override defaults",
 			env: map[string]string{
-				"SERVER_ADDRESS": "0.0.0.0:9002",
-				"BASE_URL":       "http://0.0.0.0:9002",
+				"SERVER_ADDRESS":    "0.0.0.0:9002",
+				"BASE_URL":          "http://0.0.0.0:9002",
+				"FILE_STORAGE_PATH": "./new-storage",
 			},
-			args:        []string{"cmd"},
-			wantAddress: "0.0.0.0:9002",
-			wantBaseURL: "http://0.0.0.0:9002",
+			args:                []string{"cmd"},
+			wantAddress:         "0.0.0.0:9002",
+			wantBaseURL:         "http://0.0.0.0:9002",
+			wantFileStoragePath: "./new-storage",
 		},
 		{
 			name: "env overrides flags",
 			env: map[string]string{
 				"SERVER_ADDRESS": "0.0.0.0:9999",
 			},
-			args:        []string{"cmd", "-a=127.0.0.1:9003", "-b=http://127.0.0.1:9003"},
-			wantAddress: "0.0.0.0:9999",
-			wantBaseURL: "http://127.0.0.1:9003",
+			args:                []string{"cmd", "-a=127.0.0.1:9003", "-b=http://127.0.0.1:9003"},
+			wantAddress:         "0.0.0.0:9999",
+			wantBaseURL:         "http://127.0.0.1:9003",
+			wantFileStoragePath: "./storage",
 		},
 	}
 
