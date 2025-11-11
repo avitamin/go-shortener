64f179eba8fb3e9804435f3a7d43b475ed8227fb
Антонов <vitaly.antonov@altarix.ru>
2025-11-11 21:35:22 +0400

    убрал комментарий

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -5,8 +5,6 @@ import (
 )
 
 func New() *zap.Logger {
-	// Development — человекочитаемый формат, удобно для локальной разработки.
-	// Для продакшена можно заменить на zap.NewProduction().
 	logger, _ := zap.NewDevelopment()
 	return logger
 }
